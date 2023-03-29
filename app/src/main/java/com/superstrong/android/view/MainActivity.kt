401eed19b52ce9d91dfad1a0043d914e235aaf54
조현성 <gustjd1478@korea.ac.kr>
2023-03-29 15:29:15 +0900

    [5th commit] 로그인,스플래쉬,회원가입(덜함)

--- app/src/main/java/com/superstrong/android/view/MainActivity.kt
+++ app/src/main/java/com/superstrong/android/view/MainActivity.kt
@@ -2,11 +2,5 @@ package com.superstrong.android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.superstrong.android.R
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
